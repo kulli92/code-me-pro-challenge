ae1b4f932fbcf696d2a388b11ab026a807ffe331
abdulrhman klli <abdulrhman.klli@gmail.com>
2024-02-25 14:56:51 +0800

    fix: more version change

--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,9 +1,8 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrl: './button.component.css'
 })
 export class ButtonComponent {
 
