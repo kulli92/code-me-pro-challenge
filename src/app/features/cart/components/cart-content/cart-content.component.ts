86a93ff72cdf332119dacef32b85b1d3d2f498ba
abdulrhman klli <abdulrhman.klli@gmail.com>
2024-02-25 15:05:02 +0800

    rectified build

--- src/app/features/cart/components/cart-content/cart-content.component.ts
+++ src/app/features/cart/components/cart-content/cart-content.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-cart-content',
   templateUrl: './cart-content.component.html',
-  styleUrl: './cart-content.component.css'
 })
 export class CartContentComponent {
 
